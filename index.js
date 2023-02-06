0409ca9ab559a023ad43160467d5c275ec274fd9
Pham Minh Kha <58466702+phamkha0609@users.noreply.github.com>
2023-02-06 11:38:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -51,6 +51,35 @@ bot.onText(/\/ask/, async (msg) => {
     }
 })
 
+bot.onText(/\/imagine/, async (msg) => {
+    const chatId = msg.chat.id;
+    let message = msg.text.toLowerCase();
+
+    console.log(`${msg.from.first_name}:`, message);
+    
+    if(message === "/start" || message === "/help") return;
+
+    if (message.length > 0) {
+        try {
+            if(!checkDefaultMessage(chatId, message)){
+                let response = await axios.post("https://zenithereum.vercel.app/api/image", {
+                    message: message,
+                }).then(res => res.data);
+    
+                bot.sendMessage(chatId, response.data[0]);
+            }
+        } catch (error) {
+            bot.sendMessage(chatId, "Something went wrong, please try again!");
+            const url = `https://www.elegantthemes.com/blog/wp-content/uploads/2020/02/000-404.png`;
+            bot.sendPhoto(chatId, url);
+        }
+    } else {
+        bot.sendMessage(chatId, "Please enter something!");
+        const url = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpXQgNS6hHK_Boi-oGF8yGgfH692RxCM2u3Q&usqp=CAU`;
+        bot.sendPhoto(chatId, url);
+    }
+})
+
 const checkDefaultMessage = (chatId, message) => {
     if (message.length > 0 && (message.includes("your name") || message.includes("who are you")) || message.includes("yo name") || message.includes("u name") || message.includes("who r u")) {
         bot.sendMessage(chatId, "My name is ZenithereumAI.");
