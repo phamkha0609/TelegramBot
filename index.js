ab0b5e33e57c15747c0abc09463c9de08d5ae6a5
Pham Minh Kha <genetek.technical@gmail.com>
2023-02-26 23:58:11 +0700

    first commit

--- index.js
+++ index.js
@@ -1,105 +1,53 @@
 const { default: axios } = require('axios');
 const TelegramBot = require('node-telegram-bot-api');
-const token = '5974715688:AAFNK-BEXCVAdgl54805fWYdR2Aw5kRCC8w';
+const token = '6117504816:AAEKnDLkt7QazWST5u8Al3bNc10P-wBafoY';
 const bot = new TelegramBot(token, { polling: true });
+const ethers = require('ethers');
 
-const LOCAL_DATA = new Map();
+const API_KEY = "Q79VCTTZ4SAAG1C5HZMDCHC3PXDU99EM6D";
+const ADDRESS = "0x8e14651218E7667E3f9063b5B2144A0bE1EB3195";
+let chatId = "";
+let numMint = 0;
+let detailMint = [];
+let lastArrayLenth = 0;
+let firstTime = true;
 
 bot.onText(/\/start/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Hello, I am an artificial intelligence super tool created by Zenithereum.AI You can use /help to see more information.');
+    chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Empire bot is running');
 });
 
-bot.onText(/\/help/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, `Ask me anything. I was created by Zenithereum Software Engineer
-     1. Use / zen any question in chat 
-     2. Use / img to generate a image
-    ---------------------------------
-    About us:
-        -Website: https://zenithereum.ai
-        -Twitter: https://twitter.com/zenithereum
-        -Telegram: t.me/zenithereumai`);
-});
-
-bot.onText(/\/zen/, async (msg) => {
-    const chatId = msg.chat.id;
-    let message = msg.text.toLowerCase();
-
-    console.log(`${msg.from.first_name}:`, message);
-    
-    //if(message === "/start" || message === "/help") return;
-
-    if (message.length > 0) {
+setInterval(() => {
+    setTimeout(async () => {
         try {
-            if(!checkDefaultMessage(chatId, message)){
-                let response = await axios.post("https://zenithereum.vercel.app/api/chat", {
-                    message: message,
-                }).then(res => res.data);
-    
-                bot.sendMessage(chatId, response.data);
+            if (chatId !== "") {
+                //get data from bsc
+                let res = await axios.get(`https://api.bscscan.com/api?module=logs&action=getLogs&fromBlock=0&toBlock=999999999&address=${ADDRESS}&topic0=0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef&apikey=${API_KEY}`);
+                let arr = res.data.result;
+                if(lastArrayLenth !== arr.length){
+                    numMint = arr.length - lastArrayLenth;
+                    lastArrayLenth = arr.length;
+                    detailMint = arr.slice(arr.length - numMint, arr.length);
+
+                    if(!firstTime){
+                        for (let i = 0; i < detailMint.length; i++) {
+                            let message = ``;
+                            let item = detailMint[i];
+                            let tokenId = parseInt(item.topics[3]);
+                            let minter = ethers.utils.defaultAbiCoder.decode(['address'], item.topics[2])[0];
+                            let resp = await axios.get(`https://ipfs.io/ipfs/QmTLNtLN1RVPY2sqVFDoHQgPF4QHzsqwvaJyFcpQmbrDMp/${tokenId}.json`);
+                            message += `<b>${resp.data.name} #${tokenId}</b> has been minted`;
+                            bot.sendPhoto(chatId, `https://ipfs.io/ipfs/QmTtyzT51nUbV1M9E9kJ7dH57223jrjsvvHzYip51Ut11V/${tokenId}.png`);
+                            bot.sendMessage(chatId, message, {parse_mode: 'HTML'});
+                            bot.sendMessage(chatId, `<b>Minter:</b> ${minter.substring(0, 5)}...${minter.substring(minter.length - 4, minter.length)}`, {parse_mode: 'HTML'});
+                        }
+                    }else{
+                        firstTime = false;
+                    }
+                }
             }
         } catch (error) {
-            bot.sendMessage(chatId, "Something went wrong, please try again!");
-            const url = `https://www.elegantthemes.com/blog/wp-content/uploads/2020/02/000-404.png`;
-            bot.sendPhoto(chatId, url);
+            console.log(error.message);
         }
-    } else {
-        bot.sendMessage(chatId, "Please enter something!");
-        const url = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpXQgNS6hHK_Boi-oGF8yGgfH692RxCM2u3Q&usqp=CAU`;
-        bot.sendPhoto(chatId, url);
-    }
-})
-
-bot.onText(/\/img/, async (msg) => {
-    const chatId = msg.chat.id;
-    let message = msg.text.toLowerCase();
-
-    console.log(`${msg.from.first_name}:`, message);
-    
-    //if(message === "/start" || message === "/help") return;
-
-    if (message.length > 0) {
-        try {
-            if(!checkDefaultMessage(chatId, message)){
-                let response = await axios.post("https://zenithereum.vercel.app/api/image", {
-                    message: message,
-                }).then(res => res.data);
-    
-                bot.sendPhoto(chatId, response.data[0]);
-            }
-        } catch (error) {
-            bot.sendMessage(chatId, "Something went wrong, please try again!");
-            const url = `https://www.elegantthemes.com/blog/wp-content/uploads/2020/02/000-404.png`;
-            bot.sendPhoto(chatId, url);
-        }
-    } else {
-        bot.sendMessage(chatId, "Please enter something!");
-        const url = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpXQgNS6hHK_Boi-oGF8yGgfH692RxCM2u3Q&usqp=CAU`;
-        bot.sendPhoto(chatId, url);
-    }
-})
-
-const checkDefaultMessage = (chatId, message) => {
-    if (message.length > 0 && (message.includes("your name") || message.includes("who are you")) || message.includes("yo name") || message.includes("u name") || message.includes("who r u")) {
-        bot.sendMessage(chatId, "My name is ZenithereumAI.");
-        return true;
-    }
-
-    if (message.length > 0 && message.includes("your website")) {
-        bot.sendMessage(chatId, "My website is zenithereum.ai.");
-        return true;
-    }
-
-    if (message.length > 0 && message.includes("your twitter")) {
-        bot.sendMessage(chatId, "My Twitter is https://twitter.com/zenithereum.");
-        return true;
-    }
-
-    if (message.length > 0 && (message === "hi" || message === "hello")) {
-        bot.sendMessage(chatId, "Hi, can I help you?");
-        return true;
-    }
-
-    return false;
-}
+    }, 0);
+}, 1000 * 10);
